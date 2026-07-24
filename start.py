1b0b7bbdb89be75b0b837a1d48a729737fa6e3a3
hardy495 <toni.harvy@mail.ru>
2026-07-24 14:48:04 +0300

    а

--- start.py
+++ start.py
@@ -1,5 +1,18 @@
 import subprocess
 import sys
+import os
+import threading
 
-# Запускаем один процесс - bot.py содержит оба бота
-subprocess.run([sys.executable, "bot.py"])
+def run_telegram():
+    subprocess.run([sys.executable, "bot.py"])
+
+def run_max():
+    subprocess.run([sys.executable, "max_bot.py"])
+
+if __name__ == "__main__":
+    t1 = threading.Thread(target=run_telegram, daemon=False)
+    t2 = threading.Thread(target=run_max, daemon=False)
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
